67a63d33627bc0f17331fcda1189c679cd32c60d
莫鹏程 <1669547593@qq.com>
2023-03-21 13:08:52 +0800

    修改缩进

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,55 +1,55 @@
 module.exports = {
-	extends: [ // then, enable whichever type-aware rules you want to use
-		'eslint:recommended',
-		'plugin:@typescript-eslint/recommended',
-	],
-	parser: '@typescript-eslint/parser', // add the TypeScript parser
-	plugins: [
-		'svelte3',
-		'@typescript-eslint' // add the TypeScript plugin
-	],
-	overrides: [ // this stays the same
-		{
-			files: ['*.svelte'],
-			processor: 'svelte3/svelte3'
-		}
-	],
+    extends: [ // then, enable whichever type-aware rules you want to use
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+    ],
+    parser: '@typescript-eslint/parser', // add the TypeScript parser
+    plugins: [
+        'svelte3',
+        '@typescript-eslint' // add the TypeScript plugin
+    ],
+    overrides: [ // this stays the same
+        {
+            files: ['*.svelte'],
+            processor: 'svelte3/svelte3'
+        }
+    ],
 
-	parserOptions: {
-		sourceType: 'module',
-		ecmaVersion: 2023,
-		// project: "path/to/your/tsconfig.json",
-		extraFileExtensions: ['.svelte'], // This is a required setting in `@typescript-eslint/parser` v4.24.0.
-	},
-	env: {
-		browser: true,
-		es2017: true,
-		node: true
-	},
-	settings: {
-		'svelte3/typescript': true,
-	},
-	rules: {
-		'linebreak-style': [
-			'error',
-			'unix'
-		],
-		'quotes': [
-			'error',
-			'single'
-		],
-		'semi': [
-			'error',
-			'never'
-		],
-		'prefer-const': [
-			'error',
-			{
-				'destructuring': 'all'
-			}
-		],
-		// "@typescript-eslint/no-explicit-any": "off",
-		'svelte/valid-compile': 'off',
-		"indent": ["error", 2]
-	}
+    parserOptions: {
+        sourceType: 'module',
+        ecmaVersion: 2023,
+        // project: "path/to/your/tsconfig.json",
+        extraFileExtensions: ['.svelte'], // This is a required setting in `@typescript-eslint/parser` v4.24.0.
+    },
+    env: {
+        browser: true,
+        es2017: true,
+        node: true
+    },
+    settings: {
+        'svelte3/typescript': true,
+    },
+    rules: {
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'never'
+        ],
+        'prefer-const': [
+            'error',
+            {
+                'destructuring': 'all'
+            }
+        ],
+        // "@typescript-eslint/no-explicit-any": "off",
+        'svelte/valid-compile': 'off',
+        'indent': ['error', 4]
+    }
 }
--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,13 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
 export default {
-  // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
-  // for more information about preprocessors
-  preprocess: vitePreprocess(),
-  requireConfigFile: false,
-  onwarn: (warning, handler) => {
+    // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
+    // for more information about preprocessors
+    preprocess: vitePreprocess(),
+    requireConfigFile: false,
+    onwarn: (warning, handler) => {
     // 禁用无障碍提示
-    if (warning.message.startsWith('A11y:')) return
-    handler(warning)
-  }
+        if (warning.message.startsWith('A11y:')) return
+        handler(warning)
+    }
 }
